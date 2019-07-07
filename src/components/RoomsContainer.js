47ff9ccbba3b40b71c6dc5c94f5dd151c5206785
Mia Wong <miaw629@gmail.com>
2019-07-07 15:02:24 -0400

    roomsContainer has 2 different ways to consume the context, one with <context.consumer/>, 2nd: with higher order component

---
+++ src/components/RoomsContainer.js
@@ -0,0 +1,44 @@
+import React from "react";
+// does the filtering
+import RoomsFilter from "./RoomsFilter";
+// retrieves the filtered rooms
+import RoomsList from "./RoomsList";
+import { withRoomConsumer } from "../context";
+import Loading from "./Loading";
+
+// with higher order component
+function RoomContainer({ context }) {
+    const { rooms, loading, sortedRooms } = context;
+    if (loading) {
+        return <Loading />;
+    }
+    return (
+        <div>
+            hello from rooms container
+            <RoomsFilter rooms={rooms} />
+            <RoomsList rooms={sortedRooms} />
+        </div>
+    );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// export default function RoomsContainer() {
+//     return (
+//         <RoomConsumer>
+//             {value => {
+//                 const { rooms, loading, sortedRooms } = value;
+//                 if (loading) {
+//                     return <Loading />;
+//                 }
+//                 return (
+//                     <div>
+//                         hello from rooms container
+//                         <RoomsFilter rooms={rooms} />
+//                         <RoomsList rooms={sortedRooms} />
+//                     </div>
+//                 );
+//             }}
+//         </RoomConsumer>
+//     );
+// }
