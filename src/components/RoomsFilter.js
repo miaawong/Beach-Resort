aced5b172082091ddd88449b64098606d9b7c823
Mia Wong <miaw629@gmail.com>
2019-07-08 15:33:33 -0400

    created getUnique function that takes in an array and a value, it creates a new array that do not have repeated instances of the same value. also created our select options for selecting the types of rooms. we used getunique to get the options for our rooms

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -4,7 +4,11 @@ import { useContext } from "react";
 import { RoomContext } from "../context";
 import Title from "../components/Title";
 
-export default function RoomsFilter() {
+//get all unique values
+const getUnique = (items, value) => {
+    return [...new Set(items.map(item => item[value]))];
+};
+export default function RoomsFilter({ rooms }) {
     // hooks! the way to access Context with Hooks API
     const context = useContext(RoomContext);
     const {
@@ -19,5 +23,37 @@ export default function RoomsFilter() {
         breakfast,
         pets
     } = context;
-    return <div>rooms filter</div>;
+    // get unique types
+    let types = getUnique(rooms, "type");
+    // add 'all' type to the types arr
+    types = ["all", ...types];
+    //map to jsx
+    types = types.map((type, index) => {
+        return (
+            <option value={type} key={index}>
+                {type}
+            </option>
+        );
+    });
+    return (
+        <section className="filter-container">
+            <Title title="Search Rooms" />
+            <form className="filter-form">
+                {/* select type */}
+                <div className="form-group">
+                    <label htmlFor="type">Room Type</label>
+                    <select
+                        name="type"
+                        id="type"
+                        value={type}
+                        className="form-control"
+                        onChange={handleChange}
+                    >
+                        {types}
+                    </select>
+                </div>
+                {/* end of select type */}
+            </form>
+        </section>
+    );
 }
--- src/context.js
+++ src/context.js
@@ -62,7 +62,7 @@ class RoomProvider extends Component {
         const type = event.target.type;
         const name = event.target.name;
         const value = event.target.value;
-        console.log(type, name, value);
+        console.log(`type:${type}, name:${name}, value:${value}`);
     };
     filterRooms = () => {
         console.log("hello");
